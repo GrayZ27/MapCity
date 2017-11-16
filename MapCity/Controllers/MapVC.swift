84df3b7c32bb56468003fd6f3c3cf92f7b5c927c
GrayZ27 <i520you.chen@qq.com>
2017-11-16 15:00:58 -0600

    added function to get user location authorization and set center location button

--- MapCity/Controllers/MapVC.swift
+++ MapCity/Controllers/MapVC.swift
@@ -8,28 +8,71 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
     //IBOutlets
     @IBOutlet weak var mapView: MKMapView!
     
+    //variables
+    var locationManager = CLLocationManager()
+    
+    //constants
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mapView.delegate = self
+        locationManager.delegate = self
+        
+        configureLocationService()
         
     }
     
     //IBActions
     @IBAction func centerMapBtnPresssed(_ sender: Any) {
-        
-        
-        
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapUserLocation()
+        }
     }
 
 }
 
 extension MapVC: MKMapViewDelegate {
     
+    //center the user location on map and zoom in
+    func centerMapUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
 }
+
+extension MapVC: CLLocationManagerDelegate {
+    
+    //check authorization to get user location
+    func configureLocationService() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapUserLocation()
+    }
+    
+}
+
+
+
+
+
+
+
+
