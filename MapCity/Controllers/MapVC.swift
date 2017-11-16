0ed4bbf574ef88e32c02819da28ef094518e8b77
GrayZ27 <i520you.chen@qq.com>
2017-11-16 12:47:12 -0600

    MapCity app setup

---
+++ MapCity/Controllers/MapVC.swift
@@ -0,0 +1,21 @@
+//
+//  MapVC.swift
+//  MapCity
+//
+//  Created by Gray Zhen on 11/16/17.
+//  Copyright © 2017 GrayStudio. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}
+
