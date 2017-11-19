b86bb5619b75c22b2012005c9067a755e5ce6db8
GrayZ27 <i520you.chen@qq.com>
2017-11-19 17:47:59 -0600

    added a hidden view to pull up to show images

--- MapCity/Controllers/MapVC.swift
+++ MapCity/Controllers/MapVC.swift
@@ -14,13 +14,23 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
 
     //IBOutlets
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var pullUpView: UIView!
+    @IBOutlet weak var pullUpVIewHeightConstraints: NSLayoutConstraint!
     
     //variables
     var locationManager = CLLocationManager()
     
+    //optional variables
+    var spinner: UIActivityIndicatorView?
+    var progressLbl: UILabel?
+    var collectionView: UICollectionView?
+    //needed when using collectionView programmatically
+    var flowLayout = UICollectionViewFlowLayout()
+    
     //constants
     let authorizationStatus = CLLocationManager.authorizationStatus()
     let regionRadius: Double = 1000
+    let screenSize = UIScreen.main.bounds
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,13 +41,81 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         configureLocationService()
         addDoubleTap()
         
+        //set up collectionView
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+        collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell")
+        collectionView?.delegate = self
+        collectionView?.dataSource = self
+        collectionView?.backgroundColor = #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 0.52)
+        
+        pullUpView.addSubview(collectionView!)
+        
     }
     
+    //added function when user double tap
     func addDoubleTap() {
         let doubleTap = UITapGestureRecognizer(target: self, action: #selector(dropPin(sender:)))
         doubleTap.numberOfTapsRequired = 2
         doubleTap.delegate = self
         mapView.addGestureRecognizer(doubleTap)
+    }
+    
+    //added action when user swipe down on screen
+    func addSwipeDown() {
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown))
+        swipeDown.direction = .down
+        pullUpView.addGestureRecognizer(swipeDown)
+    }
+    
+    //added spinner programmatically
+    func addSpinner() {
+        spinner = UIActivityIndicatorView()
+        spinner?.center = CGPoint(x: screenSize.width / 2 - (spinner?.frame.width)! / 2, y: 150)
+        spinner?.activityIndicatorViewStyle = .whiteLarge
+        spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        spinner?.startAnimating()
+        collectionView?.addSubview(spinner!)
+    }
+    
+    //added label programmatically
+    func addProgressLbl() {
+        progressLbl = UILabel()
+        progressLbl?.frame = CGRect(x: 0, y: 175, width: pullUpView.frame.width, height: 40)
+        progressLbl?.font = UIFont(name: "Optima Bold", size: 16)
+        progressLbl?.textColor = #colorLiteral(red: 0.2605174184, green: 0.2605243921, blue: 0.260520637, alpha: 1)
+        progressLbl?.textAlignment = .center
+        progressLbl?.text = "Downloading pictures"
+        collectionView?.addSubview(progressLbl!)
+    }
+    
+    //remove exist spinner
+    func removeSpinner() {
+        if spinner != nil {
+            spinner?.removeFromSuperview()
+        }
+    }
+    
+    func removeProgressLbl() {
+        if progressLbl != nil {
+            progressLbl?.removeFromSuperview()
+        }
+    }
+    
+    //pull up a hidden view
+    func animateViewUp() {
+        pullUpVIewHeightConstraints.constant = 300
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    //swipe down the hidden view
+    @objc func animateViewDown() {
+        pullUpVIewHeightConstraints.constant = 0
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    
     }
     
     //IBActions
@@ -73,6 +151,13 @@ extension MapVC: MKMapViewDelegate {
     //drop a pin on the map
     @objc func dropPin(sender: UITapGestureRecognizer){
         removePin()
+        removeSpinner()
+        removeProgressLbl()
+        
+        animateViewUp()
+        addSwipeDown()
+        addSpinner()
+        addProgressLbl()
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
         
@@ -84,6 +169,7 @@ extension MapVC: MKMapViewDelegate {
         
     }
     
+    //remove exist pin
     func removePin() {
         for annotation in mapView.annotations {
             mapView.removeAnnotation(annotation)
@@ -109,6 +195,31 @@ extension MapVC: CLLocationManagerDelegate {
     
 }
 
+extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    //functions must needed for collection view--------------------------------------------------------
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return 1
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 4
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell
+        
+        return cell!
+        
+    }
+    //functions must needed for collection view--------------------------------------------------------
+    
+}
 
 
 
---
+++ MapCity/Views/PhotoCell.swift
@@ -0,0 +1,22 @@
+//
+//  PhotoCell.swift
+//  MapCity
+//
+//  Created by Gray Zhen on 11/19/17.
+//  Copyright © 2017 GrayStudio. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    //required to use custom collection view cell
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
